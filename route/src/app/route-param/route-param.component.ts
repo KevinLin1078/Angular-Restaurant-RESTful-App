68fc89a7671fb29134a98e546cc010f0fa90b6fa
root <root@ip-172-31-39-163.us-east-2.compute.internal>
2019-12-11 21:02:39 +0000

    Routing

--- route/src/app/route-param/route-param.component.ts
+++ route/src/app/route-param/route-param.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute} from '@angular/router'
 @Component({
   selector: 'app-route-param',
   templateUrl: './route-param.component.html',
   styleUrls: ['./route-param.component.css']
 })
+
+
 export class RouteParamComponent implements OnInit {
+  
+  public department_id;
 
-  constructor() { }
 
+  constructor(private route : ActivatedRoute) { }
   ngOnInit() {
+    let id = parseInt(this.route.snapshot.paramMap.get('id'))
+    this.department_id = id
   }
+
 
 }
