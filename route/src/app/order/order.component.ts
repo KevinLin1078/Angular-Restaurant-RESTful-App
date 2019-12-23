bbc741cef00de63d6923f1bbf809eefe308a07cd
tl2750 <tl2750@columbia.edu>
2019-12-23 16:10:31 -0500

    good
    Tangming

--- route/src/app/order/order.component.ts
+++ route/src/app/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../service/orders.service';
 import { Service1Service} from '../service/service1.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order',
@@ -10,23 +11,26 @@ import { Service1Service} from '../service/service1.service'
 export class OrderComponent implements OnInit {
   public orders;
 
-  constructor(public OrdersService: OrdersService, public LoginService: Service1Service) { }
+  constructor(public OrdersService: OrdersService, public LoginService: Service1Service, private router: Router) { }
 
   async ngOnInit() {
     if(this.LoginService.login == false ){
       this.LoginService.go_home()
-    }else{    
+    }else{
       let response= await  this.OrdersService.get_orders()
-      response.subscribe(
-        async (response)=>{
-          console.log(response['orders'])
-          this.orders = await response['orders']
-        }
-      )
+      response.subscribe(async (response)=>{
+        console.log(response['orders'])
+        this.orders = await response['orders']
+      })
     }
   }
 
-
+  goDetail(id:number){
+    console.log("I am in goEdit")
+    this.router.navigate(['/orderdetail/'+id]);
   }
+
+
+}
 
 
--- route/src/app/orderdetail/orderdetail.component.ts
+++ route/src/app/orderdetail/orderdetail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrdersService } from '../service/orders.service';
+import { Service1Service } from '../service/service1.service';
 
 @Component({
   selector: 'app-orderdetail',
@@ -16,16 +17,21 @@ export class OrderdetailComponent implements OnInit {
   public errorMsg;
   public thisOrder;
 
-  constructor(private _orderSvc: OrdersService, private router: Router, 
+  constructor(private _orderSvc: OrdersService, public LoginService: Service1Service, private router: Router, 
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.OrderID = parseInt(this.route.snapshot.paramMap.get('id'));
-    this._orderSvc.getoneOrder(this.OrderID).subscribe(
-      (data) => this.thisOrder = data,
-      (error) => this.errorMsg = error,
-      () => console.log('the sequence completed!')
-    )
+    if(this.LoginService.login == false ){
+      this.LoginService.go_home()}
+      else{
+        this.OrderID = parseInt(this.route.snapshot.paramMap.get('id'));
+        this._orderSvc.getoneOrder(this.OrderID).subscribe(
+          (data) => this.thisOrder = data,
+          (error) => this.errorMsg = error,
+          () => console.log('the sequence completed!')
+        )
+      }
+
   }
 
   goBack(){
