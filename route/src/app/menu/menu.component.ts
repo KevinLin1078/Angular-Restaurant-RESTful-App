afdd93b99d99830ece58c172e0d370fba0291113
tl2750 <tl2750@columbia.edu>
2019-12-27 10:42:02 -0500

    sdsd
    as

--- route/src/app/menu/menu.component.ts
+++ route/src/app/menu/menu.component.ts
@@ -2,16 +2,30 @@ import { Component, OnInit} from '@angular/core';
 import { MenuService } from '../service/menu.service'
 import {ActivatedRoute} from '@angular/router'
 import { Service1Service  } from '../service/service1.service'
+import {trigger, state, style, animate, transition } from '@angular/animations'
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
+  animations: [
+    trigger('changeDivSize', [
+      state('initial', style({
+        opacity: 0,
+      })),
+      state('final', style({
+        opacity: 1,
+        backgroundColor: 'green',
+      })),
+      transition('initial=>final', animate('200ms')),
+    ]),
+  ]
 })
 export class MenuComponent implements OnInit {
   kitchen_id: number;
   dishes: any;
   kitchen_name: any;
+  currentState="initial"
 
 
 
@@ -34,12 +48,17 @@ export class MenuComponent implements OnInit {
     const formData = new FormData();
     formData.append("CSRF_TOKEN", '{{ csrf_token() }}')
     formData.append("dish_id", dish.id)
+    this.currentState = "final"
 
     let response = await this.MenuService.addToCart(formData)
     response.subscribe((resp)=>{
-      
+      this.currentState = "initial"
     })
 
+  }
+
+  delay(ms:number){
+    return new Promise(resolve => setTimeout(resolve,ms))
   }
 
 }
